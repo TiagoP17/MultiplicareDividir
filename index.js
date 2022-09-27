bde999d83e77059e1742fe119578ac94e1b14c9a
TiagoP17 <113592615+TiagoP17@users.noreply.github.com>
2022-09-27 11:11:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function multiplicapor()
+{
+        num1 = document.getElementById("numeroum").value;
+        num2 = document.getElementById("numerodois").value;
+        document.getElementById("resultado").innerHTML = num1 * num2;
+}
+
+function dividepor() 
+{ 
+        num1 = document.getElementById("numeroum").value;
+        num2 = document.getElementById("numerodois").value;
+document.getElementById("resultado").innerHTML = num1 / num2;
+}
